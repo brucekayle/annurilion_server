c038387e91b08adfb908ab09cfac499349488c2a
BruceKayle <brucekayle@163.com>
2017-08-09 21:32:06 +0800

    repair log4j warn.

--- src/main/java/com/brucekyale/controller/UserController.java
+++ src/main/java/com/brucekyale/controller/UserController.java
@@ -4,8 +4,9 @@ import com.brucekyale.model.User;
 import com.brucekyale.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +16,7 @@ import java.util.List;
  * Created by BruceKayle on 2017/8/8.
  */
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private Logger log = Logger.getLogger(UserController.class);
@@ -23,11 +24,11 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @RequestMapping("/showUser")
-    public String showUser(HttpServletRequest request, Model model){
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public List<User> showUser(){
         log.info("查询所有用户信息");
         List<User> list = userService.getAllUser();
-        model.addAttribute("userList", list);
-        return "showUser";
+        return list;
     }
 }
